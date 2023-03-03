01a58f2002e8cee89618901c791bd62891230580
tarummel <tasrummel@gmail.com>
2023-03-02 23:23:12 -0700

    deprecation comment

--- jpcore/models/kanji.py
+++ jpcore/models/kanji.py
@@ -5,7 +5,7 @@ from django.db import models
 class Kanji(models.Model):
     kanji = models.TextField()
     radicals = models.ManyToManyField('Radical')
-    strokes = models.IntegerField() # sum of radical strokes
+    strokes = models.IntegerField() # DEPRECATED in favor of kanjidic.misc.strokes
 
     def __str__(self):
         return f'kanji: {self.kanji}, rads: {self.radicals}, strokes: {self.strokes}'
